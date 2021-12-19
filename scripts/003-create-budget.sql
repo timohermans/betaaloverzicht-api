32caa546a3a48bf940e6e55648c5b05dddaef13b
Timo Hermans <tmhermans@gmail.com>
2021-12-19 14:10:53 +0100

    Restrict only one budget per month per year per category

--- scripts/003-create-budget.sql
+++ scripts/003-create-budget.sql
@@ -1,11 +1,14 @@
+drop index if exists uq_budget_month;
 drop table if exists api.budgets;
 create table api.budgets (
     id serial primary key,
-    date_budget date not null,
+    year integer not null,
+    month integer not null,
     amount decimal(10, 2) not null,
     user_id varchar(50) default api.user_id(),
     category_id integer references api.categories(id)
 );
+create unique index uq_budget_month on api.budgets (category_id, year, month);
 grant all on api.budgets to web_user;
 grant all on sequence api.budgets_id_seq to web_user;
 -- TODO: (M) Row security policy
